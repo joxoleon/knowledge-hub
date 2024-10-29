04dc93a29a1450812a0ef205dc766882105f886f
jovan.radivojsa <jovan.radivojsa@gmail.com>
2024-10-29 13:30:29 +0100

    Working version of module parser. - Updated content - Updated tests

--- KnowledgeHub/KnowledgeHub/KHContentSource/KHModuleParser.swift
+++ KnowledgeHub/KnowledgeHub/KHContentSource/KHModuleParser.swift
@@ -1,10 +1,3 @@
-//
-//  KHLearningModuleParser.swift
-//  KnowledgeHub
-//
-//  Created by Jovan Radivojsa on 28.10.24..
-//
-
 import Foundation
 import Yams
 
@@ -14,61 +7,27 @@ extension KHContentSource {
     // MARK: - Types
     
     public struct LearningModule: Codable {
+        let id: String
         let title: String
         let description: String
-        let contents: [ModuleContent]
-    }
-    
-    public enum ModuleContent: Codable {
-        case module(LearningModule)
-        case lesson(id: String)
+        let subModules: [LearningModule]
+        let lessons: [String]
         
         enum CodingKeys: String, CodingKey {
-            case title, description, contents, id
+            case title, description, subModules, lessons
         }
-        
+
         public init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
-            
-            // Determine whether this content is a `module` or `lesson`
-            if container.contains(.title) && container.contains(.description) && container.contains(.contents) {
-                // Decode as a module
-                let title = try container.decode(String.self, forKey: .title)
-                let description = try container.decode(String.self, forKey: .description)
-                let contents = try container.decode([ModuleContent].self, forKey: .contents)
-                self = .module(LearningModule(title: title, description: description, contents: contents))
-                
-            } else if container.contains(.id) {
-                // Decode as a lesson
-                let id = try container.decode(String.self, forKey: .id)
-                self = .lesson(id: id)
-                
-            } else {
-                // Handle decoding error with meaningful context
-                throw DecodingError.dataCorruptedError(
-                    forKey: .title,
-                    in: container,
-                    debugDescription: "Invalid structure: Expected either a 'module' with 'title', 'description', and 'contents' or a 'lesson' with 'id'."
-                )
-            }
-        }
-        
-        public func encode(to encoder: Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
-            
-            switch self {
-            case .module(let module):
-                try container.encode(module.title, forKey: .title)
-                try container.encode(module.description, forKey: .description)
-                try container.encode(module.contents, forKey: .contents)
-                
-            case .lesson(let id):
-                try container.encode(id, forKey: .id)
-            }
+            title = try container.decode(String.self, forKey: .title)
+            id = title.replacingOccurrences(of: " ", with: "_").lowercased()
+            description = try container.decode(String.self, forKey: .description)
+            subModules = try container.decodeIfPresent([LearningModule].self, forKey: .subModules) ?? []
+            lessons = try container.decodeIfPresent([String].self, forKey: .lessons) ?? []
         }
     }
-
-    enum ModuleParsingError: Error {
+    
+    public enum ModuleParsingError: Error {
         case fileNotFound
         case invalidContent
         case yamlDecodingFailed
