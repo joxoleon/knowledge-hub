c5f0277055126d794d693bad301a24175701d273
jovan.radivojsa <jovan.radivojsa@gmail.com>
2024-11-01 11:35:57 +0100

    Add random test for content fetching. Need to update concurrency models in KHContenSource. WIP for now

--- KHBusinessLogic/Tests/KHBusinessLogicTests/KHBusinessLogicTests.swift
+++ KHBusinessLogic/Tests/KHBusinessLogicTests/KHBusinessLogicTests.swift
@@ -1,6 +1,28 @@
 import Testing
 @testable import KHBusinessLogic
+import KHContentSource
 
 @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    await testContentRepository()
+}
+
+
+private func testContentRepository() async {
+    let contentRepository = await initializeContentRepository()
+    let modules = contentRepository.fetchAllModuleIds()
+    print("Fetched modules: \(modules)")
+    let iosModule = contentRepository.fetchModule(by: modules.first!)!
+    print("Fetched iOS Preparation module\n: \(iosModule)")
+}
+
+private func initializeContentRepository() async -> ContentRepository {
+    await withCheckedContinuation { continuation in
+        let contentFetcher = GitHubContentFetcher()
+        let contentStorage = FileContentStorage()
+        let contentRepository = ContentRepository(fetcher: contentFetcher, storage: contentStorage)
+        contentRepository.updateDataIfNeeded { updated in
+            print("Updated content repository: \(updated)")
+            continuation.resume(returning: contentRepository)
+        }
+    }
 }
